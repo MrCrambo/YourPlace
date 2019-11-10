59c46715c35346b23478c86cfe67fedc950823a6
MrCrambo <kadirov.il.crambo@gmail.com>
2019-11-11 01:36:31 +0300

    map activity

--- app/src/main/java/com/example/yourplace/MapActivity.java
+++ app/src/main/java/com/example/yourplace/MapActivity.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.WindowManager;
+import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.Objects;
@@ -18,6 +21,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     private double latitude = 0, longitude = 0;
     private String id, name, country;
+    SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +30,29 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_map);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_view);
+        Bundle bundle = getIntent().getExtras();
+        id = bundle.getString(Adapter.EXTRA_ID);
+        name = bundle.getString(Adapter.EXTRA_NAME);
+        country = bundle.getString(Adapter.EXTRA_COUNTRY);
+        latitude = bundle.getDouble(Adapter.EXTRA_LAT);
+        longitude = bundle.getDouble(Adapter.EXTRA_LON);
+
+        TextView idText = findViewById(R.id.id);
+        idText.setText("ID : " + id);
+        TextView nameText = findViewById(R.id.name);
+        nameText.setText("Name : " + name);
+        TextView countryText = findViewById(R.id.country);
+        countryText.setText("Country : " + country);
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_view);
         Objects.requireNonNull(mapFragment).getMapAsync(this);
 
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(latitude, longitude))
-                .title("Marker"));
+        Marker marker = googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title(name));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(marker.getPosition(), 11f));
     }
+
 }
