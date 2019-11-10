b696cbf52d5863da0514057bdf2919c46d7ed5b9
MrCrambo <kadirov.il.crambo@gmail.com>
2019-11-10 16:24:31 +0300

    endless adapter

--- app/src/main/java/com/example/yourplace/Adapter.java
+++ app/src/main/java/com/example/yourplace/Adapter.java
@@ -1,35 +1,103 @@
 package com.example.yourplace;
 
-import android.content.Context;
+import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import com.example.yourplace.Interface.ILoadMore;
 import java.util.List;
 
-public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+class LoadingViewHolder extends RecyclerView.ViewHolder {
 
-    private LayoutInflater layoutInflater;
+    ProgressBar progressBar;
+
+    public LoadingViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        progressBar = itemView.findViewById(R.id.progress_bar);
+    }
+}
+
+class ItemViewHolder extends RecyclerView.ViewHolder{
+
+    TextView textTitle;
+
+    public ItemViewHolder(@NonNull View itemView) {
+        super(itemView);
+        textTitle = itemView.findViewById(R.id.card_text_view);
+    }
+}
+
+public class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private final int VIEW_TYPE_ITEM = 0, VIEW_TYPE_LOADING = 1;
+
     private List<MapPointsClass> data;
+    ILoadMore loadMore;
+    private boolean isLoading;
+    private Activity activity;
+    private int visibleThreshold = 5;
+    private int lastVisibleItemPosition, totalItemsCount;
 
-    Adapter(Context context, List<MapPointsClass> data) {
-        this.layoutInflater = LayoutInflater.from(context);
+
+    Adapter(Activity activity, RecyclerView recyclerView, List<MapPointsClass> data) {
+        this.activity = activity;
         this.data = data;
+
+        LinearLayoutManager manager = (LinearLayoutManager) recyclerView.getLayoutManager();
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                assert manager != null;
+                totalItemsCount = manager.getItemCount();
+                lastVisibleItemPosition = manager.findLastVisibleItemPosition();
+                if (!isLoading && totalItemsCount < (lastVisibleItemPosition + visibleThreshold)){
+                    if (loadMore != null)
+                        loadMore.onLoadMore();
+                    isLoading = true;
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return data.get(position) == null ? VIEW_TYPE_LOADING : VIEW_TYPE_ITEM;
+    }
+
+    public void setLoadMore(ILoadMore loadMore) {
+        this.loadMore = loadMore;
     }
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View view = layoutInflater.inflate(R.layout.element_view, viewGroup,false);
-        return new ViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        if (viewType == VIEW_TYPE_ITEM){
+            View view = LayoutInflater.from(activity).inflate(R.layout.element_view, viewGroup, false);
+            return new ItemViewHolder(view);
+        } else if (viewType == VIEW_TYPE_LOADING) {
+            View view = LayoutInflater.from(activity).inflate(R.layout.loading_view, viewGroup, false);
+            return new LoadingViewHolder(view);
+        }
+        return null;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
-        String title = data.get(i).getName();
-        viewHolder.textTitle.setText(title);
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof ItemViewHolder){
+            ItemViewHolder viewHolder = (ItemViewHolder) holder;
+            viewHolder.textTitle.setText(data.get(position).getName());
+        } else if (holder instanceof LoadingViewHolder) {
+            LoadingViewHolder loadingViewHolder = (LoadingViewHolder) holder;
+            loadingViewHolder.progressBar.setIndeterminate(true);
+        }
     }
 
     @Override
@@ -37,13 +105,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
         return data.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
-
-        TextView textTitle;
-
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            textTitle = itemView.findViewById(R.id.card_text_view);
-        }
+    public void setLoaded(){
+        isLoading = false;
     }
 }
---
+++ app/src/main/java/com/example/yourplace/Interface/ILoadMore.java
@@ -0,0 +1,5 @@
+package com.example.yourplace.Interface;
+
+public interface ILoadMore {
+    void onLoadMore();
+}
