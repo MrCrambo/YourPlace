1f7e6cadcede5e672a326816e80515241c398089
MrCrambo <kadirov.il.crambo@gmail.com>
2019-11-10 14:55:03 +0300

    recyclerview and adapter added

---
+++ app/src/main/java/com/example/yourplace/MapPointsClass.java
@@ -0,0 +1,38 @@
+package com.example.yourplace;
+
+public class MapPointsClass {
+
+    private String id;
+    private String name;
+    private String country;
+    private Double lat;
+    private Double lon;
+
+    MapPointsClass(String id, String name, String country, Double lat, Double lon){
+        this.id = id;
+        this.name = name;
+        this.country = country;
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public Double getLon() {
+        return lon;
+    }
+}
