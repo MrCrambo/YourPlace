8b8066be83a16ea9e9536f5739933e77023937dc
MrCrambo <kadirov.il.crambo@gmail.com>
2019-11-10 04:05:58 +0300

    login activity

--- app/src/main/java/com/example/yourplace/LoginActivity.java
+++ app/src/main/java/com/example/yourplace/LoginActivity.java
@@ -1,14 +1,15 @@
 package com.example.yourplace;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
-
+import android.widget.Toast;
+import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -26,6 +27,7 @@ public class LoginActivity extends AppCompatActivity {
     private String password = "";
 
     private String GET_URL = "https://www.alarstudios.com/test/auth.cgi?username=%s&password=%s";
+    private static final String EXTRA_ID = "com.example.yourplace.code";
     final static Integer PORT = 443;
 
     @Override
@@ -35,6 +37,7 @@ public class LoginActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
 
+        // field for entering login
         loginEditText = findViewById(R.id.loginEditText);
         loginEditText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -51,6 +54,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        // field for entering password
         passwordEditText = findViewById(R.id.passwordEditText);
         passwordEditText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -68,53 +72,60 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         loginButton = findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!login.isEmpty() && !password.isEmpty()){
-                    try {
-                        sendGETRequest(login, password);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
+        loginButton.setOnClickListener(view -> {
+            if (!login.isEmpty() && !password.isEmpty()){
+                sendGETRequest(login, password);
+            } else if (login.isEmpty()){
+                loginEditText.setError("Login field is empty");
+            } else {
+                passwordEditText.setError("Password field is empty");
             }
         });
     }
 
-    private void sendGETRequest(String correctLogin, String correctPassword) throws Exception{
+    private void sendGETRequest(String correctLogin, String correctPassword){
 
-        final URL urlObject = new URL(String.format(GET_URL, correctLogin, correctPassword));
-        System.out.println(urlObject.getFile());
+        Thread thread = new Thread(() -> {
+            try {
+                URL urlObject = new URL(String.format(GET_URL, correctLogin, correctPassword));
+                String code = "-1";
 
-        Thread thread = new Thread(new Runnable() {
+                // socket without ssl returns 301 error, so using ssl socket for working with https
+                Socket socket = SSLSocketFactory.getDefault().createSocket(urlObject.getHost(), PORT);
 
-            @Override
-            public void run() {
-                try  {
-                    Socket socket = SSLSocketFactory.getDefault().createSocket(urlObject.getHost(), PORT);
+                PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+                printWriter.println("GET " + urlObject.getFile() + " HTTP/1.0\r\nHost: " + urlObject.getHost()+ "\r\n\r\n");
+                printWriter.flush();
 
-                    PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
-                    //String httpHeader = "GET " + urlObject.getFile() + " HTTP/1.0\r\nHost: " + urlObject.getHost()+ "\r\n\r\n";
-                    printWriter.println("GET " + urlObject.getFile() + " HTTP/1.0\r\nHost: " + urlObject.getHost()+ "\r\n\r\n");
-                    printWriter.flush();
-                    printWriter.close();
-
-                    BufferedReader response = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    String responseStr = response.readLine();
-                    while ((responseStr != null) && (responseStr != ""))
-                    {
-                        System.out.println(responseStr);
-                        responseStr = response.readLine();
+                BufferedReader response = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String responseStr = response.readLine();
+                while (responseStr != null && responseStr != "") {
+                    if (responseStr.contains("status")){
+                        JSONObject obj = new JSONObject(responseStr);
+                        if (obj.get("status").equals("ok")){
+                            code = obj.get("code").toString();
+                        }
+                        break;
                     }
-
-                } catch (Exception e) {
-                    e.printStackTrace();
+                    responseStr = response.readLine();
                 }
+                response.close();
+                printWriter.close();
+                socket.close();
+
+                if (code != "-1"){
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.putExtra(EXTRA_ID, code);
+                    startActivity(intent);
+                } else {
+                    //TODO change toast to something new
+                    runOnUiThread(() -> Toast.makeText(this, "You entered wrong login and password", Toast.LENGTH_LONG).show());
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
-
         thread.start();
     }
-
 }
--- app/src/main/java/com/example/yourplace/MainActivity.java
+++ app/src/main/java/com/example/yourplace/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.yourplace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
 
@@ -13,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        // intent to the main screen of phone
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
